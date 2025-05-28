de0ecf353c8d5ceeb174e5b1edcf435575ca91b8
ovalkonia <ovalkonia@gmail.com>
2025-05-28 23:04:24 +0300

    password hashing. NOOOOOOOOOOOOOOOOOOOOOOOOOOO, THE BUGS ARE CRAWLING ON MY SKIN, THEY ARE TRYING TO GET UNDER, HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEELP

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,7 +1,17 @@
+import bcrypt from "bcrypt";
+
 import BaseModel from "./BaseModel.js";
 
 export default class UserModel extends BaseModel {
     constructor(schema, adapter) {
         super(schema, adapter);
+    }
+
+    async hash_password(rounds) {
+        this.schema.password = await bcrypt.hash(this.schema.password, rounds);
+    }
+
+    async compare_password(password) {
+        return await bcrypt.compare(password, this.schema.password);
     }
 }
